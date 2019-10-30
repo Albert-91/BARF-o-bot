863c8d453d7f90a47528770349e35229daa7c5f6
Albert Suraliński <albert.suralinski@gmail.com>
2019-10-30 21:56:13 +0100

    Added calculate_portions_distribution function.

--- utils/products_calculator.py
+++ utils/products_calculator.py
@@ -24,3 +24,24 @@ def calculate_products_to_buy(meat_amount: Decimal) -> Dict[Text, Decimal]:
         "offal": offal_to_buy,
         "bones": bones_to_buy,
     }
+
+
+def calculate_portions_distribution(weekly_cycle: Decimal, daily_portion: Decimal) -> Dict[Text, Decimal]:
+    """
+    Function gets number of cycles and amount of daily portion and returns a dict with total weight of each product
+    in cycle.
+    :param weekly_cycle: Decimal
+    :param daily_portion: Decimal
+    :return: dictionary with values in "kg" unit of each product in cycle
+    """
+    total_cycle_food = daily_portion * Decimal(7) * weekly_cycle
+    liver_to_cycle = Decimal(ProductsRatio.LIVER.value) * total_cycle_food
+    meat_to_cycle = Decimal(ProductsRatio.MEAT.value) * total_cycle_food
+    offal_to_cycle = Decimal(ProductsRatio.OFFAL.value) * total_cycle_food
+    bones_to_cycle = Decimal(ProductsRatio.BONES.value) * total_cycle_food
+    return {
+        "liver": liver_to_cycle,
+        "meat": meat_to_cycle,
+        "offal": offal_to_cycle,
+        "bones": bones_to_cycle,
+    }

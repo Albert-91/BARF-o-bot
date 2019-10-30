0a3f138b6e788a80cc1e5d74ab85aaa9e8d5a42e
Albert Suraliński <albert.suralinski@gmail.com>
2019-10-30 21:55:42 +0100

    Refactored calculate_products_to_buy function with docstring.

--- utils/products_calculator.py
+++ utils/products_calculator.py
@@ -7,13 +7,18 @@ class ProductsRatio(Enum):
     LIVER = 0.07
     OFFAL = 0.2
     BONES = 0.3
-    MEAT = 1 - LIVER.value - OFFAL.value - BONES.value
+    MEAT = 1 - LIVER - OFFAL - BONES
 
 
 def calculate_products_to_buy(meat_amount: Decimal) -> Dict[Text, Decimal]:
-    liver_to_buy = meat_amount * Decimal(ProductsRatio.LIVER.value) / ProductsRatio.MEAT.value
-    offal_to_buy = meat_amount * Decimal(ProductsRatio.OFFAL.value) / ProductsRatio.MEAT.value
-    bones_to_buy = meat_amount * Decimal(ProductsRatio.BONES.value) / ProductsRatio.MEAT.value
+    """
+    Function gets amount of meat and depends on ratio of each product returns a dictionary with weights of all products.
+    :param meat_amount: in "kg" unit
+    :return: dictionary with values in "kg" unit of liver, offal and bones
+    """
+    liver_to_buy = meat_amount * Decimal(ProductsRatio.LIVER.value) / Decimal(ProductsRatio.MEAT.value)
+    offal_to_buy = meat_amount * Decimal(ProductsRatio.OFFAL.value) / Decimal(ProductsRatio.MEAT.value)
+    bones_to_buy = meat_amount * Decimal(ProductsRatio.BONES.value) / Decimal(ProductsRatio.MEAT.value)
     return {
         "liver": liver_to_buy,
         "offal": offal_to_buy,

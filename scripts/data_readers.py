18e6e30f92a740bbf6d783e1890a163327b9a809
Albert Suraliński <albert.suralinski@gmail.com>
2019-11-03 17:43:58 +0100

    Refactored function.

--- scripts/data_readers.py
+++ scripts/data_readers.py
@@ -7,7 +7,6 @@ def get_facebook_token_from_credentials(credential_file_path: Text) -> Text:
     with open(credential_file_path, 'r') as stream:
         try:
             data = yaml.safe_load(stream)
-            page_access_token = data['facebook']['page-access-token']
-            return page_access_token
+            return data['facebook']['page-access-token']
         except yaml.YAMLError as exc:
             print(exc)

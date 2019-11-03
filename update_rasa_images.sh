43cef74de9c44d7b8c75996a0ab4532bc5d28953
Albert Suraliński <albert.suralinski@gmail.com>
2019-11-03 21:11:41 +0100

    Updated pulling images script

--- update_rasa_images.sh
+++ update_rasa_images.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-docker pull rasa/rasa-skd:latest
+docker pull rasa/rasa-sdk:latest
 docker pull rasa/rasa:latest-full
 docker build -t barfobot_actions:latest .
+echo "Successfully pulled rasa images and built barfobot_action image."

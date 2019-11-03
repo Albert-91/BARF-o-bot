38367bb1f7beb77524339d32ec24d8ef3996172c
Albert Suraliński <albert.suralinski@gmail.com>
2019-11-03 14:06:53 +0100

    Added function to get page-access-token from credential file.

--- scripts/add_get_started_button.py
+++ scripts/add_get_started_button.py
@@ -1,30 +1,37 @@
 from typing import Text
 
+import yaml
 import json
 import requests
 
 
-def add_get_started_button(intent: Text):
+def add_get_started_button(intent_name: Text, credential_file_path: Text):
     """
     curl -X POST -H "Content-Type: application/json" -d '{
   "get_started": {"payload": "<postback_payload>"}
 }' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=<PAGE_ACCESS_TOKEN>"
 
-    :param intent:
+    :param intent_name:
     :return:
     """
-    page_access_token = ""
-    data = {
-        "get_started": {
-            "payload": "/" + intent
-        }
-    }
-    data = json.dumps(data)
-    headers = {"Content-Type": "application/json"}
+    page_access_token = get_token_from_credentials(credential_file_path)
     url = "https://graph.facebook.com/v2.6/me/messenger_profile?access_token="
+    headers = {"Content-Type": "application/json"}
+    data = {"get_started": {"payload": "/" + intent_name}}
+    data = json.dumps(data)
     r = requests.post(url=url+page_access_token, data=data, headers=headers)
     print(r.status_code)
     print(r.json())
+
+
+def get_token_from_credentials(credential_file_path: Text) -> Text:
+    with open(credential_file_path, 'r') as stream:
+        try:
+            data = yaml.safe_load(stream)
+            page_access_token = data['facebook']['page-access-token']
+            return page_access_token
+        except yaml.YAMLError as exc:
+            print(exc)
 
 
 if __name__ == '__main__':

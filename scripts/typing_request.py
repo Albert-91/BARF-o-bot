e035cd68901910b52ac261736afbce07d1e36a88
Albert Suraliński <albert.suralinski@gmail.com>
2019-12-01 23:19:37 +0100

    Changed file with typing request.

--- scripts/typing_request.py
+++ scripts/typing_request.py
@@ -1,14 +1,15 @@
 import json
+from typing import Text
 
 import requests
 
 from utils.data_readers import get_page_access_token
 
 
-def do_typing_on(psid):
+def do_typing_on(psid: Text, typing_action: Text):
     page_access_token = get_page_access_token()
     url = "https://graph.facebook.com/v2.6/me/messages?access_token="
     headers = {"Content-Type": "application/json"}
-    data = {"recipient": {"id": psid}, "sender_action": "typing_on"}
+    data = {"recipient": {"id": psid}, "sender_action": typing_action}
     data = json.dumps(data)
     requests.post(url=url+page_access_token, data=data, headers=headers)

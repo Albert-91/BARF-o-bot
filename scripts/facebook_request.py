1221067a88a641973b9cc1ba3554839de0e93c77
Albert Suraliński <albert.suralinski@gmail.com>
2019-12-05 23:05:15 +0100

    Added docstrings with example of usage for persistent menu class.

--- scripts/facebook_request.py
+++ scripts/facebook_request.py
@@ -117,6 +117,34 @@ class GetStartedButton(MessengerProfileRequest):
 class PersistentMenu(MessengerProfileRequest):
     """
     https://developers.facebook.com/docs/messenger-platform/send-messages/persistent-menu
+
+    Example of data to set:
+    {
+        "persistent_menu": [
+            {
+                "locale": "default",
+                "composer_input_disabled": false,
+                "call_to_actions": [
+                    {
+                        "type": "postback",
+                        "title": "Talk to an agent",
+                        "payload": "CARE_HELP"
+                    },
+                    {
+                        "type": "postback",
+                        "title": "Outfit suggestions",
+                        "payload": "CURATION"
+                    },
+                    {
+                        "type": "web_url",
+                        "title": "Shop now",
+                        "url": "https://www.originalcoastclothing.com/",
+                        "webview_height_ratio": "full"
+                    }
+                ]
+            }
+        ]
+    }
     """
 
     def __init__(self, token):

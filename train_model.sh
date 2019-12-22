1822ae66fe01322c9e8f06c688fe3986384e80a9
Albert Suraliński <albert.suralinski@gmail.com>
2019-12-22 23:52:10 +0100

    Changed version of rasa images.

--- train_model.sh
+++ train_model.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 chmod 777 models/model.tar.gz
-docker run -v $(pwd):/app rasa/rasa:1.4.3-full train --domain config/domain.yml --data data --config config/config.yml --out models --fixed-model-name model
+docker run -v $(pwd):/app rasa/rasa:1.6.0-full train --domain config/domain.yml --data data --config config/config.yml --out models --fixed-model-name model
